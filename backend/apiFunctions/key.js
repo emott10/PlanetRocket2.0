afe32035210013b00b94399926e8711411018ff2
William M Doudna <billdoudna@gmail.com>
2018-10-30 18:27:20 -0700

    * added a new folder to contain all of the functions for our api endpoint( will make the routes file more readable, is similar to having a controller folder)
    * Added a new mongoose model for our API keys that has an ID attribute
    and a ttl attribute
    
    * Added a POST endpoint at /api/key in our rutes api.js file
    
    * Currenlty testing the /api/key endpoint, the function for this
    endpoint compares the stored password to the received one, and if they
    match a new API key is generated, hashed, and then sent to the user
    (should we store the hashed key or send it?)
    
    * Added the random-key package for key generation

---
+++ backend/apiFunctions/key.js
@@ -0,0 +1,55 @@
+var Key = require('../models/key');
+var User = require('../models/user');
+var bcrypt = require('bcryptjs');
+var rand = require('random-key');
+
+exports.issue_key = function(req, res){
+    var postUsername = req.body.username;
+    var postPassword = req.body.password;
+
+    //see if the username matches with a stored user
+    User.findOne({username:postUsername}, function(err, result){
+        if(err){
+            console.log(err);
+            var jsonResponse = {loginSuccess: false, yourKey: null };
+            res.send(jsonResponse);
+        }
+        else{
+            //once the user is found, compare passwords
+            bcrypt.compare(postPassword, result.password, function(err, res){
+                //if the passwords match, generate an API key and send it to the client
+                if(res){
+                    console.log('passwords match');
+                    apiKey = rand.generate(32);
+                    //hash the apiKey and store it in mongo (or should we store the apikey and respond with the hash?)
+                    bcrypt.hash(apiKey, 10, function(err, apiHash){
+                        if(err){
+                            console.log(err);
+
+                        }
+                        else{
+                            //create the mongo key document with the hashed key value 
+                            Key.create({keyID: apiHash}, function(err, key){
+                                if(err){
+                                    console.log(err);
+                                }
+                                else{
+                                    //send the user the unhashed apiKey
+                                    var jsonResponse = {loginSuccess: true, yourKey: apiKey};
+                                    res.send(jsonResponse);
+                                    
+                                }
+                            });
+                            
+                        }
+                    });
+                }
+                //otherwise, return an error
+                else{
+                    console.log('passwords do not match');
+                }
+            })
+        }
+    })
+    
+}

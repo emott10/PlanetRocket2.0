833671fddd992d3812615f4f470a6ec2b65fe7eb
Will <billdoudna@gmail.com>
2018-11-04 15:22:29 -0800

    * Added get method and create method for ideas * added a method to verify api keys and return the ID of the owner of 	the key * installed bcryptjs * Created apifunction files for ideas and courses

--- backend/apiFunctions/key.js
+++ backend/apiFunctions/key.js
@@ -17,32 +17,74 @@ exports.issue_key = function(req, res){
         else{
             //once the user is found, compare passwords
             bcrypt.compare(postPassword, result.password, function(err, goodHash){
-                //if the passwords match, generate an API key and send it to the client
+                //if the passwords match, first search to see if they have any other existing api keys
                 if(goodHash){
                     console.log('passwords match');
-                    apiKey = rand.generate(32);
-                    //hash the apiKey and store it in mongo (or should we store the apikey and respond with the hash?)
-                    bcrypt.hash(apiKey, 10, function(err, apiHash){
+                    Key.findOne({ownedBy: result._id}, function(err, existingKey){
+
                         if(err){
                             console.log(err);
-
                         }
+
+                        //if there are no api keys currently assigned to the user, then create and issue a new one
+                        else if(existingKey == null){
+
+                            apiKey = rand.generate(32);
+                            //hash the apiKey and store it in mongo (or should we store the apikey and respond with the hash?)
+                            bcrypt.hash(apiKey, 10, function(err, apiHash){
+                                if(err){
+                                    console.log(err);
+        
+                                }
+                                else{
+                                    //create the mongo key document with the key value and a reference to the user that will be using the apiKey hash
+                                    Key.create({keyID: apiKey, ownedBy: result._id}, function(err, key){
+                                        if(err){
+                                            console.log(err);
+                                        }
+                                        else{
+                                            //send the user the hashed apiKey
+                                            var jsonResponse = {loginSuccess: true, yourKey: apiHash};
+                                            res.send(jsonResponse);
+                                            
+                                        }
+                                    });
+                                    
+                                }
+                            });
+                        
+                        }
+                        
+                        //if there is already an apiKey, update it its expiration time and return it
                         else{
-                            //create the mongo key document with the hashed key value and a reference to the user that will be using the apiKey
-                            Key.create({keyID: apiHash, ownedBy: result._id}, function(err, key){
+                            
+                            existingKey.createdAt = Date.now;
+                            existingKey.save(function(err){
+                                if(err){
+                                    console.log(err);
+                                }
+                            });
+
+                            //hash the key and send the hash to the user
+                            bcrypt.hash(existingKey.keyID, 10, function(err, apiHash){
                                 if(err){
                                     console.log(err);
                                 }
                                 else{
-                                    //send the user the unhashed apiKey
-                                    var jsonResponse = {loginSuccess: true, yourKey: apiKey};
+                                    var jsonResponse = {loginSuccess: true, yourKey: apiHash}; 
                                     res.send(jsonResponse);
-                                    
                                 }
                             });
-                            
+                        
+
                         }
+
                     });
+
+
+                
+                    
+                   
                 }
                 //otherwise, return an error
                 else{

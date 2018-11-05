81b09d5399a306de655d43369dd27dae5373aaf3
Levi Pole <leviwp48@gmail.com>
2018-11-04 16:01:30 -0800

    Resolved minor issue in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
-gimport React, { Component } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import LoginBox from './components/accounts/loginBox';
 import RegisterBox from './components/accounts/registerBox';
 import LoginScreen from './components/accounts/loginScreen';
+
 class App extends Component {
+
+  constructor(props){
+    super(props);
+  }
 
   render() {
     return (

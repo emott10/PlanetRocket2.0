fb37e9097074cca9931f316059da9e5f0aaa738e
Will <billdoudna@gmail.com>
2018-11-27 20:44:38 -0800

    Fixed levis break

--- frontend/src/components/dashboard/dashboard.js
+++ frontend/src/components/dashboard/dashboard.js
@@ -35,7 +35,8 @@ class Dashboard extends Component{
             modal: false,
             ideaTitle:'',
             ideaDes:'',
-            score: 0
+            score: 0,
+            ideas: []
         }
 
         this.toggle = this.toggle.bind(this);
